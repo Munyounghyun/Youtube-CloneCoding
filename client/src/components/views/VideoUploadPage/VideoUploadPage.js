fa8b09685d57b2bd30b399ea8b427844d6fe0cf6
Munyounghyun <fldh3369@naver.com>
2022-03-09 21:09:06 +0900

    유튜브 클론코딩 2

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
+
 const { TextArea } = Input;
 const { Title } = Typography;
+
+const PrivateOptions = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "Public" },
+];
+const CategoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Auto & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
+
 function VideoUploadPage() {
+  const [videoTitle, setVideoTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [category, setCategory] = useState("Film & Animation");
+
+  const onTitleChange = (e) => {
+    setVideoTitle(e.currentTarget.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value);
+  };
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+    console.log(files);
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert("비디오 업로드를 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -12,7 +62,7 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/*Drop zone*/}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={10000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
@@ -38,20 +88,28 @@ function VideoUploadPage() {
         <br />
         <br />
         <label>Title</label>
-        <Input onChange value />
+        <Input onChange={onTitleChange} value={videoTitle} />
         <br />
         <br />
         <label>Description</label>
-        <TextArea onChange value />
+        <TextArea onChange={onDescriptionChange} value={description} />
         <br />
         <br />
-        <select>
-          <option key value></option>
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
-        <select>
-          <option key value></option>
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
